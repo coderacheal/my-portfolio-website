8c837254d029790d448279c28704534b77942317
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-07 10:39:12 +0000

    Add media queries of 320px to 425px to home page

--- src/components/Home-page/ProjectCards.jsx
+++ src/components/Home-page/ProjectCards.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SeeMoreMouse from '../Utils/SeeMoreMouse';
+import projectData from '../main-nav-pages/ProjectPage/Data/projectData';
 
 const ProjectCards = () => {
+  const topThreeProjects = projectData.slice(0, 3);
+
   const [showSeeMoreMouse, setShowSeeMoreMouse] = useState(false);
 
   const handleSeeMoreMouseEnter = () => {
@@ -14,92 +17,44 @@ const ProjectCards = () => {
   };
 
   return (
-    <div>
+    <div className="homeProjectsImageAndDescription">
       <p className="ribbon-case-study">
         CASE STUDIES
       </p>
-      <div className="each-project">
-        <div className="project-details">
+      {topThreeProjects.map((eachProject) => (
+        <div className="each-project" key={eachProject.id}>
+          <div className="project-details">
+            <Link to="/projects/sunday">
+              <button
+                type="button"
+                className="projectTitle underline-reveal"
+                onMouseEnter={handleSeeMoreMouseEnter}
+                onMouseLeave={handleSeeMoreMouseLeave}
+              >
+                {eachProject.title}
+              </button>
+            </Link>
+            <p className="project-brief">
+              {eachProject.miniIntro}
+            </p>
+          </div>
           <Link to="/projects/sunday">
             <button
+              aria-label="first project"
               type="button"
-              className="projectTitle underline-reveal"
+              className="project-image-div"
               onMouseEnter={handleSeeMoreMouseEnter}
               onMouseLeave={handleSeeMoreMouseLeave}
-            >
-              SUNDAY
-            </button>
+            />
           </Link>
-          <p className="project-brief">
-            A CHAT APP EMBEDDED WITH A DEEP LEARNING BERT MODEL
-            TO PREDICT THE SENTIMENT OF YOUR TEXT BEFORE YOU SHOOT YOURSELF IN THE FOOT.
-            <u>AGAIN</u>
-          </p>
+          {/* <div className="see-more-halo">
+            <p>See</p>
+            <p>more</p>
+          </div> */}
+          <p className="see-more-halo"> See more</p>
         </div>
-        <Link to="/projects/sunday">
-          <button
-            aria-label="first project"
-            type="button"
-            className="project-image-div"
-            onMouseEnter={handleSeeMoreMouseEnter}
-            onMouseLeave={handleSeeMoreMouseLeave}
-          />
-        </Link>
-      </div>
-      <div className="each-project">
-        <Link to="/projects/everest">
-          <button
-            aria-label="second project"
-            type="button"
-            className="project-image-div"
-            onMouseEnter={handleSeeMoreMouseEnter}
-            onMouseLeave={handleSeeMoreMouseLeave}
-          />
-        </Link>
-        <div className="project-details">
-          <Link to="/projects/everest">
-            <button
-              type="button"
-              className="projectTitle underline-reveal"
-              onMouseEnter={handleSeeMoreMouseEnter}
-              onMouseLeave={handleSeeMoreMouseLeave}
-            >
-              EVEREST
-            </button>
-            {' '}
+      ))}
 
-          </Link>
-          <p className="project-brief">
-            A FINANCE APP THAT ENABLES YOU TO TRACK YOUR
-            EXPENSES BASED ON THE TYPE OF EXPENSES YOU DEFINE. COMPLETE WITH AUTHENTICATION,
-            IT HELPS YOU STAY ON TOP OF THE DOLLAR
-          </p>
-        </div>
-      </div>
-      <div className="each-project">
-        <div className="project-details">
-          <Link to="/projects/waterfall">
-            <button
-              type="button"
-              className="projectTitle underline-reveal"
-              onMouseEnter={handleSeeMoreMouseEnter}
-              onMouseLeave={handleSeeMoreMouseLeave}
-            >
-              WATER FALL
-            </button>
-          </Link>
-          <p className="project-brief">I CERTIFED AS A SCRUM MASTER TO BECOME A BETTER TEAM PLAYER AND DECIDED TO BUILD A PRODUCT MANAGEMENT TOOL FOR MY COMPANY WITH PURE JAVASCRIPT</p>
-        </div>
-        <Link to="/projects/waterfall">
-          <button
-            aria-label="third project"
-            type="button"
-            className="project-image-div"
-            onMouseEnter={handleSeeMoreMouseEnter}
-            onMouseLeave={handleSeeMoreMouseLeave}
-          />
-        </Link>
-      </div>
       <div className="">
         <div
           type="button"
