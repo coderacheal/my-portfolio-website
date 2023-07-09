8c694efb5352cf501055a603289fc96c70669b75
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-09 15:26:48 +0000

    Add media querie to contact page

--- src/components/main-nav-pages/ContactPage.jsx
+++ src/components/main-nav-pages/ContactPage.jsx
@@ -11,9 +11,9 @@ const ContactPage = () => (
         <div>
           <p className="in-touch-comment-large">GET IN</p>
           <p className="in-touch-comment-large">TOUCH</p>
-          <a href="https://github.com/coderacheal" className="in-touch-comment-mini">HELLO@RACHEALAPPIAHKUBI.COM</a>
+          <a href="https://github.com/coderacheal" className="in-touch-comment-mini">HELLO@RACHEAL</a>
           <br />
-          {/* <a href="https://github.com/coderacheal" className="in-touch-comment-mini">APPIAHKUBI.COM</a> */}
+          <a href="https://github.com/coderacheal" className="in-touch-comment-mini">APPIAHKUBI.COM</a>
           <p className="in-touch-comment">A FULL STACK DEVELOPER WITH A BACKGROUND IN DATA SCIENCE AND MACHINE LEARNING</p>
         </div>
       </div>
