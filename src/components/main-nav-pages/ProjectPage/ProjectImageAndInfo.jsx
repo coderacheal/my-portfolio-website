7ad81668f6d417c8dee839a32518ac322590adb1
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-16 00:56:02 +0000

    Add greyout effect to project page

--- src/components/main-nav-pages/ProjectPage/ProjectImageAndInfo.jsx
+++ src/components/main-nav-pages/ProjectPage/ProjectImageAndInfo.jsx
@@ -8,6 +8,7 @@ import projectData from './Data/projectData';
 /* eslint-disable  react/jsx-one-expression-per-line */
 
 const ProjectImageAndInfo = () => {
+  const [hoveredImage, setHoveredImage] = useState(null);
   const [scrollLeft, setScrollLeft] = useState(0);
 
   const handleScroll = (e) => {
@@ -20,18 +21,32 @@ const ProjectImageAndInfo = () => {
     }
   };
 
+  const handleImageHover = (index) => {
+    setHoveredImage(index);
+  };
+
+  const handleImageLeave = () => {
+    setHoveredImage(null);
+  };
+
   return (
     <div>
       <div className={projects.allProjectsImages} onWheel={handleScroll}>
         {projectData.map((project, index) => (
           <div
-            className={projects.projectPlusInfo}
+            className={`${projects.projectPlusInfo} ${hoveredImage !== null && hoveredImage !== index ? projects.greyedOut : ''}`}
             key={project.id}
+            onMouseEnter={() => handleImageHover(index)}
+            onMouseLeave={handleImageLeave}
           >
             <Link to={`/work/${project.urlExtension}`}>
               <div>
                 <p>0{index + 1}</p>
-                <img src={project.projectImage} alt="" className={projects.projectImage} />
+                <img
+                  src={project.projectImage}
+                  alt=""
+                  className={`${projects.projectImage} ${hoveredImage === index ? projects.hoveredImg : ''}`}
+                />
                 <p className={projects.projecImageTitle}>{project.title}</p>
               </div>
             </Link>
@@ -41,4 +56,5 @@ const ProjectImageAndInfo = () => {
     </div>
   );
 };
+
 export default ProjectImageAndInfo;
