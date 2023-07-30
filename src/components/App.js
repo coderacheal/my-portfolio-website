2010ec582e2e1f53ab6390419fa21dd69f39b17d
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-30 22:41:17 +0000

    Add add correct links for project

--- src/components/App.js
+++ src/components/App.js
@@ -24,55 +24,79 @@ const ScrollToTop = () => {
 };
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
+  const [showLoader, setShowLoader] = useState(true);
 
   const lenis = new Lenis();
 
-  lenis.on('scroll', (e) => {
-    console.log(e);
-  });
-
-  lenis.on('scroll', (e) => {
-    console.log(e);
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
-
   useEffect(() => {
-    // Simulating an asynchronous task
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 100000);
+    lenis.on('scroll', (e) => {
+      console.log(e);
+    });
+
+    lenis.on('scroll', (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    window.addEventListener('load', () => {
+      // Set the showLoader state to false when the page has finished loading
+      setShowLoader(false);
+    });
   }, []);
 
   return (
     <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <div>
-              {isLoading ? (<div className="loader"><Loader duration={3000} /></div>
-              ) : (
-                <div className="content"><HomePage /></div>)}
-            </div>
-        )}
-        />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/work" element={<ProjectsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/work/sage" element={<Sage />} />
-        <Route path="/work/schoolx" element={<SchoolX />} />
-        <Route path="/work/everest" element={<Everest />} />
-      </Routes>
+      {showLoader ? (
+        <div className="loader">
+          <Loader duration={3000} />
+        </div>
+      ) : (
+        <div className="content">
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/work" element={<ProjectsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/work/sage" element={<Sage />} />
+            <Route path="/work/schoolx" element={<SchoolX />} />
+            <Route path="/work/everest" element={<Everest />} />
+          </Routes>
+        </div>
+      )}
     </Router>
   );
+
+  // return (
+  //   <Router>
+  //     <ScrollToTop />
+  //     <Routes>
+  //       <Route
+  //         path="/"
+  //         element={(
+  //           <div>
+  //             {isLoading ? (<div className="loader"><Loader duration={3000} /></div>
+  //             ) : (
+  //               <div className="content"><HomePage /></div>)}
+  //           </div>
+  //       )}
+  //       />
+  //       <Route path="/about" element={<AboutPage />} />
+  //       <Route path="/work" element={<ProjectsPage />} />
+  //       <Route path="/contact" element={<ContactPage />} />
+  //       <Route path="/work/sage" element={<Sage />} />
+  //       <Route path="/work/schoolx" element={<SchoolX />} />
+  //       <Route path="/work/everest" element={<Everest />} />
+  //     </Routes>
+  //   </Router>
+  // );
 };
 
 export default App;
