8315261762ea697073d7d9841994fac2981b3ff7
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-08-31 18:57:25 +0000

    Trying to use `react-slick` for project, but it is not working

--- src/components/Indivual-Projects/components/ProjectCarousel.jsx
+++ src/components/Indivual-Projects/components/ProjectCarousel.jsx
@@ -3,45 +3,56 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 // import newImage from '../../../assets/budget.png';
 import PropTypes from 'prop-types';
-import carousel from '../../../styles/projects/carousel.module.css';
+// import carousel from '../../../styles/projects/carousel.module.css';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import { CustomNextArrow, CustomPrevArrow } from './CarouselArrows';
+
+import projects from '../../main-nav-pages/ProjectPage/Data/projectData'
 
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-
-const ProjectCarousel = ({ firstCarouselImage, secondCarouselImage, thirdCarouselImage }) => {
+// firstCarouselImage, secondCarouselImage, thirdCarouselImage ,
+const ProjectCarousel = () => {
   const [selectedItem, setSelectedItem] = useState(0);
-  const carouselRef = useRef(null);
+  // const carouselRef = useRef(null);
 
-  const handleImageClick = (index) => {
-    setSelectedItem(index);
+
+  // const handleImageClick = (index) => {
+  //   setSelectedItem(index);
+  // };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
   };
 
   return (
-    <Carousel
-      selectedItem={selectedItem}
-      onChange={setSelectedItem}
-      showThumbs={false}
-      showStatus={false}
-      ref={carouselRef}
-      className={carousel.carouselDiv}
-    >
-      <div onClick={() => handleImageClick(1)}>
-        <img src={firstCarouselImage} alt="First Project" className={carousel.oneProjectImage} />
-      </div>
-      <div onClick={() => handleImageClick(2)}>
-        <img src={secondCarouselImage} alt="Second Project" className={carousel.oneProjectImage} />
-      </div>
-      <div onClick={() => handleImageClick(0)}>
-        <img src={thirdCarouselImage} alt="Third Project" className={carousel.oneProjectImage} />
-      </div>
-    </Carousel>
+    <div className="sliderDiv">
+      <Slider {...settings} className="slider">
+        {projects.map((project) => (
+          <div>
+            {project.carouselImages.map((image, index) => (
+              <img key={index} src={image} alt={`carousel image ${index}`} />
+            ))}
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
 ProjectCarousel.propTypes = {
-  firstCarouselImage: PropTypes.string.isRequired,
-  secondCarouselImage: PropTypes.string.isRequired,
-  thirdCarouselImage: PropTypes.string.isRequired,
+  carouselImages: PropTypes.string.isRequired,
+  // firstCarouselImage: PropTypes.string.isRequired,
+  // secondCarouselImage: PropTypes.string.isRequired,
+  // thirdCarouselImage: PropTypes.string.isRequired,
 };
 
 export default ProjectCarousel;
